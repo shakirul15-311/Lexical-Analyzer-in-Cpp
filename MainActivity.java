5ab00ad0b1fb3d05a85580eb8b9dbbc81f457751
Shakirul Islam <shakirul15-311@diu.edu.bd>
2017-09-26 20:38:21 +0600

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.shakirul.kabbokothon;
+
+import android.support.v4.view.KeyEventCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView view; //membuat variabel view agar bisa akses method onKeyDown
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view = (WebView) this.findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.setWebViewClient(new MyBrowser());
+        view.loadUrl("http://Kabbokothon01.blogspot.com"); //try js alert
+        view.setWebChromeClient(new WebChromeClient()); // adding js alert support
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //ketika disentuh tombol back
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && view.canGoBack()) {
+            view.goBack(); //method goback() dieksekusi untuk kembali pada halaman sebelumnya
+            return true;
+        }
+        // Jika tidak ada history (Halaman yang sebelumnya dibuka)
+        // maka akan keluar dari activity
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
